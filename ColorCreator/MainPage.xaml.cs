e1c933393b8bfa89901049dce7644037d2c93d27
poked <poked@LAPTOP-SHO65KA2>
2023-04-22 22:37:28 -0600

    Agregar archivos de proyecto.

---
+++ ColorCreator/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace ColorCreator;
+
+public partial class MainPage : ContentPage
+{
+	bool israndom;
+	string hexValue;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+		if (!israndom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+            Color color = Color.FromRgb(red, green, blue);
+
+            SetColor(color);
+        }
+		
+    }
+
+	private void SetColor(Color color)
+	{
+		//
+		//btn.BackgroundColor = color;
+		btnRandoom.BackgroundColor = color;
+		Container.BackgroundColor = color;
+		hexValue = color.ToHex();
+		lblHex.Text = hexValue;
+	}
+
+    private void btnRandoom_Clicked(object sender, EventArgs e)
+    {
+		israndom = true;
+		var random = new Random();
+		var color = Color.FromRgb(random.Next(0, 256), 
+			random.Next(0, 256), 
+			random.Next(0, 256));
+		SetColor(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+		israndom = false;
+
+    }
+
+    private async void btn_Clicked(object sender, EventArgs e)
+    {
+		await Clipboard.SetTextAsync(hexValue);
+		var toast = Toast.Make("Color Copiado",
+			CommunityToolkit.Maui.Core.ToastDuration.Short,
+			12);
+		await toast.Show();
+    }
+}
+
+
+
